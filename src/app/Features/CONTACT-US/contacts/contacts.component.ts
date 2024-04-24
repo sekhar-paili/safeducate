c6800c7c296de3a3edd0e7266adc359d606d9aa4
sekhar.paili@safexpress.com-at-533108483701 <sekhar.paili@safexpress.com>
2024-04-24 15:45:51 +0530

    created common service

--- src/app/Features/CONTACT-US/contacts/contacts.component.ts
+++ src/app/Features/CONTACT-US/contacts/contacts.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonService } from '../../../Core/common.service';
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
   imports: [],
+  providers: [CommonService],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
